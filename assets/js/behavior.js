826d149199c3dbdc02289d5fd84fd72f4460c84d
Oggy <ognjen.djokic@gmail.com>
2020-04-14 14:11:50 +0200

    first commit

---
+++ assets/js/behavior.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+    //MAIN NAV
+    var navTrigger = $('.pr-nav-button');
+    var nav = $('.pr-container-nav');
+    navTrigger.on('click', function () {
+        nav.toggleClass('hide-menu');
+        $('.pr-wrapper-right').toggleClass('wrapper-right-expand');
+    });
+
+    //TOP BAR SCROLL
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = $('.pr-top-bar').outerHeight();
+
+    $(window).scroll(function (event) {
+        didScroll = true;
+    });
+    setInterval(function () {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 100);
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+        if (Math.abs(lastScrollTop - st) <= delta)
+            return;
+        if (st > lastScrollTop && st > navbarHeight) {
+            $('.pr-nav-button').removeClass('nav-down').addClass('nav-up');
+            $('.pr-top-bar').removeClass('nav-down').addClass('nav-up');
+        } else {
+            if (st + $(window).height() < $(document).height()) {
+                $('.pr-nav-button').removeClass('nav-up').addClass('nav-down');
+                $('.pr-top-bar').removeClass('nav-up').addClass('nav-down');
+            }
+        }
+        lastScrollTop = st;
+    }
+
+    //BLOCK MENU
+    $('.pr-block-menu').on('click', function () {
+        var trigger = event.target;
+        $(trigger).find('.menu-items').toggle();
+    });
+    $('.menu-items').on('click', function() {
+        $(this).hide();
+    });
+    $(document).mouseup(function (ev) {
+        var container = $('.menu-items');
+        if (!container.is(ev.target) && container.has(ev.target).length === 0) {
+            container.hide();
+        }
+    });
+});
